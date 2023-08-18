5c551cabad26935d4d4b50cb2120a9dea9d25b6c
m.alizadeh <alizadee.maedeh@gmail.com>
2023-08-18 18:50:11 +0330

    Added Scroll to top

--- src/app.js
+++ src/app.js
@@ -8,21 +8,26 @@ import ProductDetails from './components/ProductDetails';
 import Navbar from './components/shared/Navbar';
 import ShopCart from './components/ShopCart';
 import HomePage from './components/HomePage';
+import Footer from './components/shared/Footer';
+import ScrollToTop from './components/features/ScrollToTop';
 
 // Redux
 import store from './redux/store';
 
 function App() {
+
   return (
     <Provider store={store}>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/products/:id" element={<ProductDetails/>} />
         <Route path="/products" element={<Store/>} />
         <Route path="/cart" element={<ShopCart/>} />
-        <Route path='/' element={ <HomePage/> } />
+        <Route path="/" element={ <HomePage/> } />
         <Route path="/*" element={<Navigate to="/"/>} />
       </Routes>
+      <Footer />
     </Provider>
   );
 }
